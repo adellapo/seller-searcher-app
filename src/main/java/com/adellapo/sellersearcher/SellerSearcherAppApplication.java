cc5b6633402eff53711e9d762498b6c737bca38c
unknown <andres.m.dellaporta@gmail.com>
2020-09-05 14:32:47 -0300

    Creación del proyecto

---
+++ src/main/java/com/adellapo/sellersearcher/SellerSearcherAppApplication.java
@@ -0,0 +1,13 @@
+package com.adellapo.sellersearcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellerSearcherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellerSearcherAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adellapo/sellersearcher/SellerSearcherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adellapo.sellersearcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellerSearcherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
